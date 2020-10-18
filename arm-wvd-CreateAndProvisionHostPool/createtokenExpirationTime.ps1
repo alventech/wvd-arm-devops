b5cfab8507770cdaacdd8c31533a18f185ba246b
steinalven <36982793+steinalven@users.noreply.github.com>
2020-10-18 19:21:14 +0200

    add createtokenExpirationTime powershell scirpt

---
+++ arm-wvd-CreateAndProvisionHostPool/createtokenExpirationTime.ps1
@@ -0,0 +1,9 @@
+﻿# ARM tokenExpirationTime
+# The expiration date can be no less than an hour and no more than one month. 
+# If you set tokenExpirationTime outside of that limit, it wont work. 
+# tokenExpirationTime is used to generate token for WVD hostpool
+
+
+$ExpirationTime = $((get-date).ToUniversalTime().AddDays(1).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ'))
+
+Write-Host "$("##vso[task.setvariable variable=ExpirationTime]") $($ExpirationTime)"
