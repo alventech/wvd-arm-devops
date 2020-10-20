e445151c9dd270dcc936fcbf44d39a42c1de5931
steinalven <36982793+steinalven@users.noreply.github.com>
2020-10-20 12:07:35 +0200

    fix days to hours in ExpirationTime

--- arm-wvd-CreateAndProvisionHostPool/createhostpoolExpirationTime.ps1
+++ arm-wvd-CreateAndProvisionHostPool/createhostpoolExpirationTime.ps1
@@ -1,9 +1,9 @@
-﻿# ARM tokenExpirationTime
+﻿# ARM tokenExpirationTime Used by arm parameter
 # The expiration date can be no less than an hour and no more than one month. 
 # If you set tokenExpirationTime outside of that limit, it wont work. 
 # tokenExpirationTime is used to generate token for WVD hostpool
 
-
-$ExpirationTime = $((get-date).ToUniversalTime().AddDays(1).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ'))
+$ExpirationTime = $((get-date).ToUniversalTime().Addhours(2).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ'))
 
 Write-Host "$("##vso[task.setvariable variable=ExpirationTime]") $($ExpirationTime)"
+
